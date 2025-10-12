b71aa83ac0732f3bb1f910c13c5cf1d776600da3
Juli Albrecht <juliella2002@gmail.com>
2025-10-12 15:06:50 +0200

    add styles

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 const submitButton = document.querySelector("[data-js=submitButton]");
-const form = document.querySelector('[data-js="form"]');
+const form = document.querySelector('[data-js="new-card-form"]');
+const cardContainer = document.querySelector('[data-js="card"]');
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-  const formElement = event.target.elements;
-  console.log(formElement.inputQuestion.value);
-  console.log(formElement.inputAnswer.value);
-  console.log(formElement.inputTag.value);
 
   const formData = new FormData(event.target);
-
   const data = Object.fromEntries(formData);
   console.log("user data:", data);
+
+  const newCard = document.createElement("section");
+  newCard.classList.add("card");
+
+  const questionHeading = document.createElement("h2");
+  questionHeading.innerText = data.inputQuestion;
+  newCard.append(questionHeading);
+
+  cardContainer.append(newCard);
+  // console.log(newListElement);
+
   event.target.reset();
 });
